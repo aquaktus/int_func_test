702008d4a21b849e4b9cbdc0cc2a8701765d9afa
Sivakumaran S <siva-kumaran@users.noreply.github.com>
2018-06-08 16:01:40 +0100

    Update lambda_function.py

--- func_tests/src/lambda_function.py
+++ func_tests/src/lambda_function.py
@@ -7,4 +7,5 @@ def lambda_handler(event, context):
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
     print("Hi there...!")
+    print("Fionaaaaaaaaa!!!!!!!")
     return event['key1']  # Echo back the first key value

e4203e97631d22aa9adab4d548de424bc5775c19
Sivakumaran S <siva-kumaran@users.noreply.github.com>
2018-06-11 11:24:50 +0100

    Update lambda_function.py
    import boto3

--- func_tests/src/lambda_function.py
+++ func_tests/src/lambda_function.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import json
 from datetime import datetime
+import boto3
 print('Loading function')
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))

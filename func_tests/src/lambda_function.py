466896b6f2cab0c3d4923773529dfb4908ca9cb3
Sivakumaran S <siva-kumaran@users.noreply.github.com>
2018-06-11 16:17:21 +0100

    Update lambda_function.py
    Updated code to include success and failure based on if-else statement

--- func_tests/src/lambda_function.py
+++ func_tests/src/lambda_function.py
@@ -13,18 +13,26 @@ def lambda_handler(event, context):
     except KeyError as err:
         print("Could not retrieve CodePipeline Job ID!\n%s", err)
     print("Received event: " + json.dumps(event, indent=2))
+    ft_passed = True
     # If all functional tests passed, call PutJobSuccessResult with this job id.
     # Else, PutJobFailureResult with this job id.
-    try:
+    if ft_passed:
         codepipeline = boto3.client('codepipeline')
-        #codepipeline.put_job_success_result(jobId=job_id)
-        #print('SUCCESS!!!!: All functional tests passed')
-        codepipeline.put_job_failure_result(jobId=job_id, failureDetails={
-        'type': 'JobFailed',
-        'message': 'Functional Tests failed',
-        'externalExecutionId': 'string'})
-        print('FAILURE!!!!: Some functional tests failed')
-        return True
-    except ClientError as err:
-        LOGGER.error("Failed to PutJobSuccessResult for CodePipeline!\n%s", err)
-        return False
+        try:  
+            codepipeline.put_job_success_result(jobId=job_id)
+            print('SUCCESS!!!!: All functional tests passed')
+            return True
+        except ClientError as err:
+            print("Failed to PutJobSuccessResult for CodePipeline!\n%s", err)
+            return False
+    else:
+        try:
+            codepipeline.put_job_failure_result(jobId=job_id, failureDetails={
+            'type': 'JobFailed',
+            'message': 'Functional Tests failed',
+            'externalExecutionId': 'string'})
+            print('FAILURE!!!!: Some functional tests failed')
+            return True
+        except ClientError as error:
+            print("Failed to PutJobFailureResult for CodePipeline!\n%s", err)
+            return False

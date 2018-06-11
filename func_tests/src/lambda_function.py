90426d21a72995ff84344692dcc11e279c6c2365
Sivakumaran S <siva-kumaran@users.noreply.github.com>
2018-06-11 11:29:24 +0100

    Update lambda_function.py
    Added code to get job_id and put_job_success_result

--- func_tests/src/lambda_function.py
+++ func_tests/src/lambda_function.py
@@ -4,6 +4,11 @@ from datetime import datetime
 import boto3
 print('Loading function')
 def lambda_handler(event, context):
+    try:
+        job_id = event['CodePipeline.job']['id']
+    except KeyError as err:
+        LOGGER.error("Could not retrieve CodePipeline Job ID!\n%s", err)
+        return False
     print("Received event: " + json.dumps(event, indent=2))
     #print("value1 = " + event['key1'])
     #print("value2 = " + event['key2'])
@@ -14,3 +19,11 @@ def lambda_handler(event, context):
     print(str(datetime.now()))
     #return event['key1']  # Echo back the first key value
     return "Success"
+    try:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_success_result(jobId=job_id)
+        LOGGER.info('===SUCCESS===')
+        return True
+    except ClientError as err:
+        LOGGER.error("Failed to PutJobSuccessResult for CodePipeline!\n%s", err)
+        return False

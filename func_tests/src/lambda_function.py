4d81bb524e7a8aafe718241cb4728fece5709b9e
Sivakumaran S <siva-kumaran@users.noreply.github.com>
2018-06-11 15:18:34 +0100

    Update lambda_function.py
    Testing PutJobFailureResult

--- func_tests/src/lambda_function.py
+++ func_tests/src/lambda_function.py
@@ -5,15 +5,20 @@ import boto3
 from botocore.exceptions import ClientError
 print('Loading function')
 def lambda_handler(event, context):
+    # Get the job id so that we can update the result of this lambda and codepipeline
+    # can capture it
     try:
         job_id = event['CodePipeline.job']['id']
         print(str(datetime.now()), ":", str(job_id))
     except KeyError as err:
         print("Could not retrieve CodePipeline Job ID!\n%s", err)
     print("Received event: " + json.dumps(event, indent=2))
+    # If all functional tests passed, call PutJobSuccessResult with this job id.
+    # Else, PutJobFailureResult with this job id.
     try:
         codepipeline = boto3.client('codepipeline')
-        codepipeline.put_job_success_result(jobId=job_id)
+        #codepipeline.put_job_success_result(jobId=job_id)
+        codepipeline.put_job_failure_result(jobId=job_id)
         print('SUCCESS: All functional tests passed')
         return True
     except ClientError as err:
